d1e8dfba93ecd1781d006e56d2fb322187e98b60
Neil <neil@skywire.co.uk>
2020-09-10 09:22:18 +0100

    use @actions/github to get commit log

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 let lint = require('@skywire-london/conventional-changelog-lint');
 
-const commit_log = core.getInput('namespaces');
-console.log(commit_log);
-let contents = fs.readFileSync(commit_log, 'utf8');
-let commits = contents.split("\n")
-console.log(commits);
+const payload = github.context.payload;
+const token = core.getInput('token');
+const octokit = github.getOctokit(token)
 
+octokit.pulls.listCommits({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    pull_number: payload.pull_request.number,
+}).then((response) => {
+    let errors = [];
 
+    response.data.forEach(function(commit) {
+
+        let sha = commit.sha;
+        let message = commit.commit.message.split("\n").shift();
+
+        if (!lint.validateFormat(message)) {
+            try {
+                lint.lint(message);
+            } catch (e) {
+                let logMessage = `${sha}: ${message}: ${e.toString()}`;
+                errors.push(logMessage);
+                core.error(logMessage);
+            }
+        }
+    });
+
+    if(errors.length) {
+        core.setFailed(`PR contains ${errors.length} invalid commit messages`);
+    }
+
+}).catch((error) => {
+    core.error(error);
+})
