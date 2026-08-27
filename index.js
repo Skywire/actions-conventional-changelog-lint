23fa092d7f735c8e4f6640fd744508770fefda6a
Tom Scott-Malden <tom@skywire.co.uk>
2026-08-27 16:48:51 +0100

    SKY-000: refactor: improve code formatting and structure in index.js

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-let lint = require('@skywire-london/conventional-changelog-lint');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const lint = require("@skywire-london/conventional-changelog-lint");
 
 const payload = github.context.payload;
-const token = core.getInput('token');
-const octokit = github.getOctokit(token)
+const token = core.getInput("token");
+const octokit = github.getOctokit(token);
 
-octokit.pulls.listCommits({
-    owner: payload.repository.owner.login,
-    repo: payload.repository.name,
-    pull_number: payload.pull_request.number,
-}).then((response) => {
-    let errors = [];
+octokit.pulls
+	.listCommits({
+		owner: payload.repository.owner.login,
+		repo: payload.repository.name,
+		pull_number: payload.pull_request.number,
+	})
+	.then((response) => {
+		const errors = [];
 
-    response.data.forEach(function(commit) {
+		response.data.forEach((commit) => {
+			const sha = commit.sha;
+			const message = commit.commit.message.split("\n").shift();
 
-        let sha = commit.sha;
-        let message = commit.commit.message.split("\n").shift();
+			if (message.startsWith("Merge pull request")) {
+				return;
+			}
 
-        if (message.startsWith('Merge pull request')) {
-            return;
-        }
+			if (!lint.validateFormat(message)) {
+				try {
+					lint.lint(message);
+				} catch (e) {
+					const logMessage = `${sha}: ${message}: ${e.toString()}`;
+					errors.push(logMessage);
+					core.error(logMessage);
+				}
+			}
+		});
 
-        if (!lint.validateFormat(message)) {
-            try {
-                lint.lint(message);
-            } catch (e) {
-                let logMessage = `${sha}: ${message}: ${e.toString()}`;
-                errors.push(logMessage);
-                core.error(logMessage);
-            }
-        }
-    });
-
-    if(errors.length) {
-        core.setFailed(`PR contains ${errors.length} invalid commit messages`);
-    }
-
-}).catch((error) => {
-    core.error(error);
-})
+		if (errors.length) {
+			core.setFailed(`PR contains ${errors.length} invalid commit messages`);
+		}
+	})
+	.catch((error) => {
+		core.error(error);
+	});
